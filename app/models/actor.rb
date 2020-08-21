deffb9df1d96b13965d5f4f978004c8c6ade1265
mizzygonzo <xeligon@gmail.com>
2020-08-21 11:17:43 -0500

    90 minute mark

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-    
+    has_many :roles 
+    #binding.pry
+    has_many :movies, through: :roles 
 end
+
+
+# x- `Actor#roles`
+#   x- should return a collection of all the roles that the actor has played
+# x- `Actor#movies`
+#   x- should return a collection of all the movies that the actor has performed in
+
+# (i.e. `has_many`, `has_many through`, and `belongs_to`
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,19 @@
 class Role < ActiveRecord::Base
-    
+    belongs_to :actor
+    belongs_to :movie 
+
+    def credit
+        puts "#{self}: Played by #{actor}"
+    end 
 end
+
+
+# x- `Role#actor`
+#   x- should return the `Actor` instance for this role
+# x- `Role#movie`
+#   x- should return the `Movie` instance for this role
+
+# x- `Role#credit`
+#   x- should return a string formatted as follows: 
+#   `{insert character name}: Played by {insert actor name}`
+
